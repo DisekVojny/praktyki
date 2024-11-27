6d9db5ea724214783a660c2b430e4733b4f82871
DisekVojny <alex.hrycek@gmail.com>
2024-11-27 11:34:05 +0100

    general improvemnts

--- src/components/Hint.vue
+++ src/components/Hint.vue
@@ -1,29 +1,41 @@
 <script setup lang="ts">
-const props = defineProps(['answer'])</script>
+import { ref, watch } from 'vue';
+
+const props = defineProps(['answer', 'showModal']);
+const emit = defineEmits(['update:showModal']);
+
+const isActive = ref(false);
+
+watch(() => props.showModal, (newValue) => {
+  isActive.value = newValue;
+});
+
+function closeModal() {
+  isActive.value = false;
+  emit('update:showModal', false);
+}
+</script>
 
 <template>
-  <v-dialog max-width="500">
+  <v-dialog v-model="isActive" max-width="500">
     <template v-slot:activator="{ props: activatorProps }">
       <button class="hintButton" v-bind="activatorProps">Hint</button>
     </template>
 
-    <template v-slot:default="{ isActive }">
-      <v-card class="modal" :title="props.answer">
+    <template v-slot:default>
+      <v-card class="modal">
+        <v-card-title>{{ answer }}</v-card-title>
         <v-card-actions>
-          <v-btn
-            class="close"
-            text="Close"
-            @click="isActive.value = false"
-          ></v-btn>
+          <v-btn class="close" text="Close" @click="closeModal">Close</v-btn>
         </v-card-actions>
       </v-card>
     </template>
   </v-dialog>
 </template>
 
-<style scoped type="scss">
-.v-card{
-    background-color: #1e1e2e;
-    border-radius: 10px !important;
+<style scoped>
+.v-card {
+  background-color: #1e1e2e;
+  border-radius: 10px !important;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
-import {getPair} from '../logic.ts'
-import Hint from "./Hint.vue";
+import { onMounted, onUnmounted, ref } from 'vue';
+import { getPair } from '../logic.ts';
+import Hint from './Hint.vue';
 
-const input = ref("");
-const qa = ref(getPair())
+const input = ref('');
+const qa = ref(getPair());
+const showModal = ref(false);
 
 function check() {
-  if (input.value == qa.value[1]) {
-    input.value = "";
+  if (input.value === qa.value[1]) {
+    input.value = '';
     qa.value = getPair();
   }
 }
+
+function handleKeyboardShortcut(event: KeyboardEvent) {
+  if (event.ctrlKey && event.code === 'Space') {
+    showModal.value = true;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyboardShortcut);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyboardShortcut);
+});
 </script>
 
 <template>
@@ -19,8 +34,7 @@ function check() {
     <div class="info">{{ qa[0] }}</div>
     <div class="buttons">
       <input class="input" type="text" v-model="input" @input="check" placeholder="Message..." autofocus>
-      <!-- <button class="hintButton" @click="hint">Hint</button> -->
-       <Hint :answer="qa[1]"></Hint>
+      <Hint :answer="qa[1]" v-model:showModal="showModal" />
     </div>
   </div>
 </template>
